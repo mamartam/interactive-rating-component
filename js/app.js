21dc38ca37d0ad3f118ab3d3f38b2edcdafc4fd7
mamartam <marta.masiukk@gmail.com>
2026-03-09 15:11:58 +0200

    add script for form - when user clicks submit main section bdecomes hidden and user sees 'thank you' section

--- js/app.js
+++ js/app.js
@@ -8,26 +8,45 @@ const ratingRormInputsContainer = document.querySelector(
   ".rating__form--inputs",
 );
 const componentRatingSection = document.querySelector(".component__rating");
+const componentThankYouStateSection = document.querySelector(
+  ".component__thank-you-state",
+);
+const userChoice = document.querySelector(".user-choice");
 
 let userRating = null;
 ratingRormInputsContainer.addEventListener("click", (event) => {
   if (event.target.name === "rating") {
     userRating = event.target.value;
-    console.log(userRating);
+    let labelsArray = event.currentTarget.querySelectorAll(
+      ".rating__form--label",
+    );
+    Array.from(labelsArray).forEach((element) => {
+      element.classList.remove("selected");
+    });
+    let el = Array.from(labelsArray).find((element) => {
+      if (element.dataset.id === event.target.id) {
+        return element;
+      }
+    });
+    el.classList.add("selected");
   }
 });
 
 ratingForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  console.log(userRating);
   if (userRating === null) {
     errorMessage.classList.remove("hide");
+    setTimeout(() => {
+      errorMessage.classList.add("hide");
+    }, 3000);
   } else {
+    userChoice.textContent = userRating;
     errorMessage.classList.add("hide");
     componentRatingSection.classList.add("hide");
+    componentThankYouStateSection.classList.remove("hide");
   }
 });
 
-errorMessageCloseBtn.addEventListener("click", (event) => {
+errorMessageCloseBtn.addEventListener("click", () => {
   errorMessage.classList.add("hide");
 });
